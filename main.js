b371f01c95fb44c20b450fdce1e68bdc501c8a9b
slemenevaviyaleta <slemenevaviyaleta@gmail.com>
2023-06-18 12:55:40 +0200

    Solved deepEqual function

--- main.js
+++ main.js
@@ -120,6 +120,58 @@ for (let i = 0; i < array.length; i++) {
 console.log(newArray);
 
 
+const obj1 = {
+    a: 'a',
+    b: {
+        a: 'a',
+        b: 'b',
+        c: {
+            a: 1,
+        },
+    },
+};
+const obj2 = {
+    b: {
+        c: {
+            a: 1,
+        },
+        b: 'b',
+        a: 'a',
+    },
+    a: 'a',
+};
+const obj3 = {
+    a: {
+        c: {
+            a: 'a',
+        },
+        b: 'b',
+        a: 'a',
+    },
+    b: 'b',
+};
+
+
+const deepEqual = (object1, object2) => {
+    if (object1 === object2)
+        return true;
+    if ((typeof object1 !== "object") || (typeof object2 !== "object") || (object1 === null) || (object2 === null))
+        return false;
+    const keys1 = Object.keys(object1);
+    const keys2 = Object.keys(object2);
+    if (keys1.length !== keys2.length)
+        return false;
+    for (let i = 0; i < keys1.length; i++) {
+        const key = keys1[i];
+        if (!keys2.includes(key) || !deepEqual(object1[key], object2[key]))
+            return false;
+    }
+    return true;
+};
+
+console.log(deepEqual(obj1, obj2)); // true
+console.log(deepEqual(obj1, obj3)); // false
+
 
 
 
