d2dab383e79a9a294893de2be6ff2b99760c60f6
slemenevaviyaleta <slemenevaviyaleta@gmail.com>
2023-07-25 19:00:32 +0200

    Added display of filters from the design

--- main.js
+++ main.js
@@ -33,13 +33,13 @@ const carousel = {
     next() {
         if (this.index >= this.data.length) {
             this.done = true;
-            return { done: true };
+            return {done: true};
         }
 
         const imageUrl = this.data[this.index].imageUrl;
         this.index++;
 
-        return { value: imageUrl, done: false };
+        return {value: imageUrl, done: false};
     },
 };
 
@@ -52,7 +52,7 @@ function showNextImage() {
     }
 
     const img = document.createElement('img');
-    const { value, done } = carousel.next();
+    const {value, done} = carousel.next();
 
     if (!done) {
         img.src = value;
@@ -61,3 +61,111 @@ function showNextImage() {
 }
 
 btnEllipse.addEventListener('click', showNextImage);
+
+//////////////
+
+const guestField = document.getElementById('guestField');
+const filterWrapper = document.querySelector('.filter__wrapper');
+
+guestField.addEventListener('click', function () {
+    filterWrapper.classList.toggle('js__hidden');
+});
+
+
+const adultsPlusBtn = document.querySelector('.plus__adults');
+const adultsNumElem = document.querySelector('.adults__number');
+const adultsMinBtn = document.querySelector('.minus__adults');
+
+let adultsNum = 0;
+
+adultsPlusBtn.addEventListener('click', function () {
+    if (adultsNum < 30) {
+        adultsNum += 1;
+        adultsNumElem.textContent = adultsNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+});
+
+adultsMinBtn.addEventListener('click', function () {
+    if (adultsNum > 1) {
+        adultsNum -= 1;
+        adultsNumElem.textContent = adultsNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+});
+
+const childPlusBtn = document.querySelector('.plus__child');
+const childMinBtn = document.querySelector('.minus__child');
+const childNumElem = document.querySelector('.child__number');
+const selectElements = document.querySelectorAll('.filter__select');
+
+
+let childNum = 0;
+
+childPlusBtn.addEventListener('click', function () {
+    if (childNum < 10) {
+        childNum += 1;
+        childNumElem.textContent = childNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+});
+
+childMinBtn.addEventListener('click', function () {
+    if (childNum > 0) {
+        childNum -= 1;
+        childNumElem.textContent = childNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+});
+
+function updateSelectVisibility() {
+    for (let i = 0; i < selectElements.length; i++) {
+        if (i < childNum) {
+            selectElements[i].classList.remove('js__hidden');
+        } else {
+            selectElements[i].classList.add('js__hidden');
+        }
+    }
+}
+
+const roomPlusBtn = document.querySelector('.plus__room');
+const roomMinBtn = document.querySelector('.minus__room');
+const roomNumElem = document.querySelector('.room__number');
+
+let roomNum = 0;
+
+roomPlusBtn.addEventListener('click', function () {
+    if (roomNum < 30) {
+        roomNum += 1;
+        roomNumElem.textContent = roomNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+});
+
+roomMinBtn.addEventListener('click', function () {
+    if (roomNum > 1) {
+        roomNum -= 1;
+        roomNumElem.textContent = roomNum;
+    }
+    updateCounts(adultsNum, childNum, roomNum);
+})
+
+function updateCounts(adultsCount, childrenCount, roomCount) {
+    const inputValue = `${adultsCount} Adults — ${childrenCount} Children — ${roomCount} Room`;
+    document.getElementById('guests').value = inputValue;
+    updateSelectVisibility();
+}
+
+updateCounts(adultsNum, childNum, roomNum);
+
+
+
+
+
+
+
+
+
+
+
+
